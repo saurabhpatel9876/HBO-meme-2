6fa03464320636210c8037cf411e0cd0ebb3f37b
saurabh patel <saurabhpatel5061456@gmail.com>
2024-10-08 20:52:26 +0530

    8th

--- src/app/About/page.js
+++ src/app/About/page.js
@@ -4,11 +4,15 @@ const page = () => {
   return (
     <>
     <p className="font-bold m-4 p-2">
-    What is HBO, you ask? Well, it's not your typical cryptocurrency. This is Honorable Bitcoin Overlord, the coin that does not just want to be part of the blockchain—it wants to rule it! We are on a mission to unveil the one true Overlord of the crypto universe. Could it be Satoshi? Or is it you, secretly typing away on your keyboard? Nobody knows... yet.
+    What is HBO, you ask? 
+    <br/>
+    Well, it is not your typical cryptocurrency. This is Honorable Bitcoin Overlord, the coin that does not just want to be part of the blockchain—it wants to rule it! We are on a mission to unveil the one true Overlord of the crypto universe. Could it be Satoshi? Or is it you, secretly typing away on your keyboard? Nobody knows... yet.
 
-But one thing is for sure: $HBO isn't just a memecoin, it's a movement. A movement powered by laughs, vibes, and the occasional chaotic energy of the internet. We will be running polls, gathering theories, and creating a cosmic alliance of believers. Who will the Overlord be? Vote, meme, and hold on to your $HBO, because the truth is out there!
+But one thing is for sure: $HBO is not just a memecoin, it is a movement.
+<br/>
+ A movement powered by laughs, vibes, and the occasional chaotic energy of the internet. We will be running polls, gathering theories, and creating a cosmic alliance of believers. Who will the Overlord be? Vote, meme, and hold on to your $HBO, because the truth is out there!
 
-Join us in the ultimate quest for the grand reveal, and remember—if you're holding $HBO, you're not just a holder, you're part of the legend.
+Join us in the ultimate quest for the grand reveal, and remember—if you're holding $HBO, you're not just a holder, you are part of the legend.
 
 
     </p>
--- src/app/HBOMore/page.js
+++ src/app/HBOMore/page.js
@@ -4,12 +4,21 @@ const page = () => {
   return (
     <p className="font-bold m-4 p-1">
         About HBO: The Honorable Bitcoin Overlord
-What up, internet wanderers? Welcome to the world of HBO Honorable Bitcoin Overlord, the memecoin that's about to change the game. Forget about boring old tokens with no personality; we have got vibes, polls, and more speculation than an HBO documentary on who the heck Satoshi Nakamoto is!
+        <br/>
+Whats up, internet wanderers? 
+<br/>
 
-Yeah, you heard that right. While the other HBO is out here teasing you with a high-budget documentary about uncovering Satoshi true identity, we are one step ahead. Spoiler alert: the real Overlord might already be among us... Is it you? Is it your neighbor's cat? Could it even be your grandma who's been secretly stacking sats? 🧑‍💻👵🐱
+Welcome to the world of HBO Honorable Bitcoin Overlord, the memecoin that is about to change the game. Forget about boring old tokens with no personality; we have got vibes, polls, and more speculation than an HBO documentary on who the heck Satoshi Nakamoto is!
 
-But we are not just here for the conspiracy theories. With $HBO,you are joining a community that is as unpredictable as crypto prices. We will be running polls to decide who is the rightful Bitcoin Overlord—and spoiler alert, it's probably not the guy on that documentary. Along the way, we're vibing, memeing, and keeping it weird.
+Yeah, you heard that right. While the other HBO is out here teasing you with a high-budget documentary about uncovering Satoshi true identity, we are one step ahead. Spoiler alert: the real Overlord might already be among us... Is it you? Is it your neighbor  cat?
+<br/>
+ Could it even be your grandma who has been secretly stacking sats? 🧑‍💻👵🐱
+ <br/>
 
+But we are not just here for the conspiracy theories. With $HBO,you are joining a community that is as unpredictable as crypto prices. We will be running polls to decide who is the rightful Bitcoin Overlord—and spoiler alert, it is probably not the guy on that documentary. Along the way, we are vibing, memeing, and keeping it weird.
+
+<br/>
+<br/>
 Who is the true Overlord of crypto? Is it Satoshi? Is it someone else entirely? FIND OUT SOON... when you buy $HBO, cast your vote, and join the quest for the ultimate meme-fueled reveal.
 
 Ready to vibe with the Overlord? Grab your popcorn, hodl your $HBO, and stay tuned. Things are about to get wild. 🍿
