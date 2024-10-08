0819179e54abda928317fe85dcbb86d4d5cd88df
saurabh patel <saurabhpatel5061456@gmail.com>
2024-10-08 20:54:39 +0530

    9th

--- src/app/About/page.js
+++ src/app/About/page.js
@@ -7,12 +7,12 @@ const page = () => {
     What is HBO, you ask? 
     <br/>
     Well, it is not your typical cryptocurrency. This is Honorable Bitcoin Overlord, the coin that does not just want to be part of the blockchain—it wants to rule it! We are on a mission to unveil the one true Overlord of the crypto universe. Could it be Satoshi? Or is it you, secretly typing away on your keyboard? Nobody knows... yet.
-
+<br/>
 But one thing is for sure: $HBO is not just a memecoin, it is a movement.
 <br/>
  A movement powered by laughs, vibes, and the occasional chaotic energy of the internet. We will be running polls, gathering theories, and creating a cosmic alliance of believers. Who will the Overlord be? Vote, meme, and hold on to your $HBO, because the truth is out there!
 
-Join us in the ultimate quest for the grand reveal, and remember—if you're holding $HBO, you're not just a holder, you are part of the legend.
+Join us in the ultimate quest for the grand reveal, and remember—if you are holding $HBO, you are not just a holder, you are part of the legend.
 
 
     </p>
